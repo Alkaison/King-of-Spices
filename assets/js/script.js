aa01d58fd1b04465e7552cd660e0696429159248
Alkaison <505ganeshmourya@gmail.com>
2023-04-09 01:00:56 +0530

    Updated: NavBar Style, Header images & context

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// NavBar Scroll Toggle 
+window.addEventListener("scroll", () => {
+    var header = document.querySelector(".nav-main");
+    header.classList.toggle("active", window.scrollY > 100);
+});
