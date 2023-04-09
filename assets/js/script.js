2a5ecb5f3c0e6222556ee60543e1f30f71494c4e
Alkaison <505ganeshmourya@gmail.com>
2023-04-09 12:34:25 +0530

    Added: Footer Section and Copyright

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@
 // NavBar Scroll Toggle 
 window.addEventListener("scroll", () => {
     var header = document.querySelector(".nav-main");
-    header.classList.toggle("active", window.scrollY > 100);
+    header.classList.toggle("active", window.scrollY > 20);
 });
