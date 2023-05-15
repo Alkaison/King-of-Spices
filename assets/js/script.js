68dc9b5bef73732bd1ea0569d20d42f3ffb69f8b
Alkaison <505ganeshmourya@gmail.com>
2023-05-15 23:27:13 +0530

    Fix: nav and header section responsive css

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,8 @@ const openMenu = document.querySelector(".openMenu");
 const closeMenu = document.querySelector(".closeMenu");
 const menu_items = document.querySelectorAll(".nav-ul li a");
 
-menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    });
+menu_items.forEach((item) => {
+    item.addEventListener("click",close);
 });
 
 openMenu.addEventListener("click", show);
@@ -32,11 +30,6 @@ function close()
     mainMenu.style.top = "-100vh";
 }
 
-// Reset Form Input Fields 
-function resetForm() {
-    form.reset();
-}
-
 // Book A Table Form 
 const form = document.querySelector('form');
 const submitButton = document.querySelector('input[type="submit"]');
@@ -46,7 +39,7 @@ form.addEventListener('submit', (event) => {
 
     if (form.checkValidity()) {
         alert("Thanks for reserving your seats, soon you will receive a message on your Phone Number & Mail ID for the confirmations!");
-        resetForm();
+        form.reset();
     } else {
         alert("Please fill out all required fields!");
     }
